aa3154ba884fa47a236eac8af71c82df2536b9f1
liam-clowes <clowesliam@gmail.com>
2024-04-19 13:11:28 +0200

    app_2_push_19/04/24

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ df_cities =df_monthly_agg[df_monthly_agg['city'].isin(['Berlin', 'Tokyo', 'Welli
 # Create a table for Berlin - TO USE
 table1 = dash_table.DataTable(df_berlin.to_dict('records'),
                                   [{"name": i, "id": i} for i in df_berlin.columns],
-                               style_data={'color': 'white','backgroundColor': "#222222"},
+                               style_data={'color': '#fd7e14','backgroundColor': "#060606"},
                               style_header={
                                   'backgroundColor': 'rgb(210, 210, 210)',
                                   'color': 'black','fontWeight': 'bold'}, 
                                      style_table={ 
-                                         'minHeight': '600px', 'height': '600px', 'maxHeight': '600px',
+                                         'minHeight': '500px', 'height': '500px', 'maxHeight': '500px',
                                          'minWidth': '900px', 'width': '900px', 'maxWidth': '900px',
                                          'marginLeft': 'auto', 'marginRight': 'auto',
                                          'marginTop': 0, 'marginBottom': "20"}
@@ -42,8 +42,8 @@ fig1 = px.bar(df_berlin,
               color='avg_temp_c',
               color_continuous_scale='Peach',
               title='Average Monthly Temperature in Berlin - April 2023 to April 2024',
-              height=400,
-              width=800)
+              height=500,
+              width=1100)
 
 # Adjust temperature scale
 fig1.update_traces(marker_coloraxis=None)  # Remove automatic coloring of bars based on temperature
@@ -59,9 +59,9 @@ fig1.update_xaxes(tickangle=45)  # Rotate x-axis labels by 45 degrees
 
 # Update layout for better readability
 fig1.update_layout(
-    plot_bgcolor="#222222",
-    paper_bgcolor="#222222",
-    font_color="white",
+    plot_bgcolor="#060606",
+    paper_bgcolor="#060606",
+    font_color="#fd7e14",
     #margin=dict(l=80, r=20, t=20, b=20)  # Adjust margin to provide more space for y-axis labels
 )
 
@@ -70,12 +70,12 @@ graph1 = dcc.Graph(figure=fig1)
 # Create a table for Tokyo - TO USE
 table2 = dash_table.DataTable(df_tokyo.to_dict('records'),
                                   [{"name": i, "id": i} for i in df_tokyo.columns],
-                               style_data={'color': 'white','backgroundColor': "#222222"},
+                               style_data={'color': '#fbc02d','backgroundColor': "#060606"},
                               style_header={
                                   'backgroundColor': 'rgb(210, 210, 210)',
                                   'color': 'black','fontWeight': 'bold'}, 
                                      style_table={ 
-                                         'minHeight': '600px', 'height': '600px', 'maxHeight': '600px',
+                                         'minHeight': '500px', 'height': '500px', 'maxHeight': '500px',
                                          'minWidth': '900px', 'width': '900px', 'maxWidth': '900px',
                                          'marginLeft': 'auto', 'marginRight': 'auto',
                                          'marginTop': 0, 'marginBottom': "20"}
@@ -88,8 +88,8 @@ fig2 = px.bar(df_tokyo,
               color='avg_temp_c',
               color_continuous_scale='Peach',
               title='Average Monthly Temperature in Tokyo - April 2023 to April 2024',
-              height=400,
-              width=800)
+              height=500,
+              width=1100)
 
 # Adjust temperature scale
 fig2.update_traces(marker_coloraxis=None)  # Remove automatic coloring of bars based on temperature
@@ -105,9 +105,9 @@ fig2.update_xaxes(tickangle=45)  # Rotate x-axis labels by 45 degrees
 
 # Update layout for better readability
 fig2.update_layout(
-    plot_bgcolor="#222222",
-    paper_bgcolor="#222222",
-    font_color="white",
+    plot_bgcolor="#060606",
+    paper_bgcolor="#060606",
+    font_color="#fbc02d",
     #margin=dict(l=80, r=20, t=20, b=20)  # Adjust margin to provide more space for y-axis labels
 )
 
@@ -116,26 +116,26 @@ graph2 = dcc.Graph(figure=fig2)
 # Create a table for Wellington - TO USE
 table3 = dash_table.DataTable(df_wellington.to_dict('records'),
                                   [{"name": i, "id": i} for i in df_wellington.columns],
-                               style_data={'color': 'white','backgroundColor': "#222222"},
+                               style_data={'color': '#ffcc80','backgroundColor': "#060606"},
                               style_header={
                                   'backgroundColor': 'rgb(210, 210, 210)',
                                   'color': 'black','fontWeight': 'bold'}, 
                                      style_table={ 
-                                         'minHeight': '600px', 'height': '600px', 'maxHeight': '600px',
+                                         'minHeight': '500px', 'height': '500px', 'maxHeight': '500px',
                                          'minWidth': '900px', 'width': '900px', 'maxWidth': '900px',
                                          'marginLeft': 'auto', 'marginRight': 'auto',
                                          'marginTop': 0, 'marginBottom': "20"}
                                      )
 
-# Create a bar graph for Tokyo
+# Create a bar graph for Wellington
 fig3 = px.bar(df_wellington,
               x='month_year',
               y='avg_temp_c',
               color='avg_temp_c',
               color_continuous_scale='Peach',
               title='Average Monthly Temperature in Wellington - April 2023 to April 2024',
-              height=400,
-              width=800)
+              height=500,
+              width=1100)
 
 # Adjust temperature scale
 fig3.update_traces(marker_coloraxis=None)  # Remove automatic coloring of bars based on temperature
@@ -151,9 +151,9 @@ fig3.update_xaxes(tickangle=45)  # Rotate x-axis labels by 45 degrees
 
 # Update layout for better readability
 fig3.update_layout(
-    plot_bgcolor="#222222",
-    paper_bgcolor="#222222",
-    font_color="white",
+    plot_bgcolor="#060606",
+    paper_bgcolor="#060606",
+    font_color="#ffcc80",
     #margin=dict(l=80, r=20, t=20, b=20)  # Adjust margin to provide more space for y-axis labels
 )
 
@@ -164,13 +164,13 @@ fig4 = px.bar(df_temp,
               x='date',
               y='avg_temp_c',
               color='city',
-              color_continuous_scale='Peach',
+              color_discrete_map={'Berlin': '#fd7e14', 'Tokyo': '#fbc02d', 'Wellington': '#ffcc80'},
               range_y=[-10, 70],
               title='Average Daily Temperatures in all locations - April 2023 to April 2024',
               height=600,
-              width=800)
+              width=1100)
 fig4 = fig4.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white"
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white"
     )
 graph4 = dcc.Graph(figure=fig4)
 
@@ -178,13 +178,15 @@ graph4 = dcc.Graph(figure=fig4)
 fig5 = px.line(df_cities, 
               x='month_year', 
               y='avg_temp_c', 
-              height=500, 
+              height=500,
+              width=1100,
               title="Average Monthly Temperatures: April 2023 - April 2024, All Locations", 
               markers=True,
-              color='city'
+              color='city',
+              color_discrete_map={'Berlin': '#fd7e14', 'Tokyo': '#fbc02d', 'Wellington': '#ffcc80'}
              )
 fig5 = fig5.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white"
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white"
     )
 graph5 = dcc.Graph(figure=fig5)
 
@@ -194,11 +196,13 @@ fig6 = px.scatter(df_cities,
                  y='max_temp_c',
                  size = 'max_temp_c',
                  height=400,
+                 width=1100,
                  color='city',
+                 color_discrete_map={'Berlin': '#fd7e14', 'Tokyo': '#fbc02d', 'Wellington': '#ffcc80'},
                  title="Maximum Monthly Temperatures: Berlin, Tokyo, Wellington"
              )
 fig6 = fig6.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white"
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white"
     )
 graph6 = dcc.Graph(figure=fig6)
 
@@ -215,17 +219,17 @@ fig7 = px.scatter(df_cities,
                  animation_group="month_year",  # Use month_year to slow down the animation and progress chronologically
                  size="avg_temp_c", 
                  color='city',
-                 color_continuous_scale='Viridis',
+                 color_discrete_map={'Berlin': '#fd7e14', 'Tokyo': '#fbc02d', 'Wellington': '#ffcc80'},
                  range_y=[-15, 40],
                  title="Average Monthly Temperatures: April 2023 - April 2024, All Locations",
                  height=500, 
-                 width=800)
+                 width=1100)
 
 # Customize x-axis ticks
 fig7 = fig7.update_xaxes(ticktext=df_cities['month_year'], #.dt.strftime('%b %Y'),  # Format tick labels as abbreviated month and year
                  tickangle=45)  # Rotate tick labels by 45 degrees
 fig7 = fig7.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white")
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white")
 
 graph7= dcc.Graph(figure=fig7)
 
@@ -240,43 +244,39 @@ fig8 = px.scatter_mapbox(df_geo2,
                         hover_name='city', 
                         size='avg_temp_c',
                         color='city',
-                        color_discrete_sequence=['#636EFA'],
+                        color_discrete_map={'Berlin': '#fd7e14', 'Tokyo': '#fbc02d', 'Wellington': '#ffcc80'},
     
                         # start location and zoom level
                         zoom=4, 
                         center={'lat': 51.1657, 'lon': 10.4515}, 
                         mapbox_style='carto-positron',
                         height=500, 
-                        width=800)
+                        width=1100)
 
 fig8 = fig8.update_layout(
-        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white")
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white")
 
 graph8 = dcc.Graph(figure=fig8)
 
 # Creating my dash app - the skeleton
 
-app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
-
-#DO NOT FORGET TO ADD SERVER=APP, SERVER
-server = app.server
-
+app = dash.Dash(external_stylesheets=[dbc.themes.CYBORG])
 app.layout = html.Div([
-    html.H1('Week 08 - Project Challenge - Weather API', style={'textAlign': 'center', 'color': '#636EFA'}),
+    html.H1('Week 08 - Project Challenge - Weather API', style={'textAlign': 'center', 'color': '#e65100'}),
     html.Div(html.H3("Liam Clowes"), style={'textAlign': 'center', 'color': '#636EFA'}),
     html.Div(html.P("Checking weather in Berlin, Tokyo and Wellington to identify ideal time to organise an outside event"), style={'textAlign': 'center'}),
     html.Div([
-        html.Div('Berlin', style={'backgroundColor': 'violet', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
+        html.Div('Berlin', style={'backgroundColor': '#fd7e14', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
         table1,
         graph1,
     ]),
     html.Div([
-        html.Div('Tokyo', style={'backgroundColor': 'lemonchiffon', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
+        html.Div('Tokyo', style={'backgroundColor': '#fbc02d', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
         table2,
         graph2,
     ]),
     html.Div([
-        html.Div('Wellington', style={'backgroundColor': 'goldenrod', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
+        html.Div('Wellington', style={'backgroundColor': '#ffcc80', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
         table3,
         graph3,
     ]),
