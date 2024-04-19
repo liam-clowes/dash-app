8cdaba62465f2fa9f6e2d8170edf6eff7b246c35
liam-clowes <clowesliam@gmail.com>
2024-04-19 14:45:03 +0200

    dash_push_3_19/04/24

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ fig4 = px.bar(df_temp,
               height=600,
               width=1100)
 fig4 = fig4.update_layout(
-        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white"
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="#ffad8e"
     )
 graph4 = dcc.Graph(figure=fig4)
 
@@ -186,7 +186,7 @@ fig5 = px.line(df_cities,
               color_discrete_map={'Berlin': '#fd7e14', 'Tokyo': '#fbc02d', 'Wellington': '#ffcc80'}
              )
 fig5 = fig5.update_layout(
-        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white"
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="#ffad8e"
     )
 graph5 = dcc.Graph(figure=fig5)
 
@@ -202,7 +202,7 @@ fig6 = px.scatter(df_cities,
                  title="Maximum Monthly Temperatures: Berlin, Tokyo, Wellington"
              )
 fig6 = fig6.update_layout(
-        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white"
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="#ffad8e"
     )
 graph6 = dcc.Graph(figure=fig6)
 
@@ -229,7 +229,7 @@ fig7 = px.scatter(df_cities,
 fig7 = fig7.update_xaxes(ticktext=df_cities['month_year'], #.dt.strftime('%b %Y'),  # Format tick labels as abbreviated month and year
                  tickangle=45)  # Rotate tick labels by 45 degrees
 fig7 = fig7.update_layout(
-        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white")
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="#ffad8e")
 
 graph7= dcc.Graph(figure=fig7)
 
@@ -245,7 +245,7 @@ fig8 = px.scatter_mapbox(df_geo2,
                         size='avg_temp_c',
                         color='city',
                         color_discrete_map={'Berlin': '#fd7e14', 'Tokyo': '#fbc02d', 'Wellington': '#ffcc80'},
-    
+                        title="Scatter Map of Average Monthly Temperatures: April 2023 - April 2024, All Locations",
                         # start location and zoom level
                         zoom=4, 
                         center={'lat': 51.1657, 'lon': 10.4515}, 
@@ -254,7 +254,7 @@ fig8 = px.scatter_mapbox(df_geo2,
                         width=1100)
 
 fig8 = fig8.update_layout(
-        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="white")
+        plot_bgcolor="#060606", paper_bgcolor="#060606", font_color="#ffad8e")
 
 graph8 = dcc.Graph(figure=fig8)
 
@@ -265,30 +265,36 @@ app = dash.Dash(external_stylesheets=[dbc.themes.CYBORG])
 #Do not forget to add the server=app.server!!!
 server = app.server
 
+# Set fixed width for tables and graphs
+table_graph_width = '800px'
+
 app.layout = html.Div([
     html.H1('Week 08 - Project Challenge - Weather API', style={'textAlign': 'center', 'color': '#e65100'}),
     html.Div(html.H3("Liam Clowes"), style={'textAlign': 'center', 'color': '#636EFA'}),
     html.Div(html.P("Checking weather in Berlin, Tokyo and Wellington to identify ideal time to organise an outside event"), style={'textAlign': 'center'}),
     html.Div([
-        html.Div('Berlin', style={'backgroundColor': '#fd7e14', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
-        table1,
-        graph1,
+        html.Div('Berlin', style={'backgroundColor': '#fd7e14', 'color': 'black'}),
+        html.Div(table1, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        html.Div(graph1, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
     ]),
     html.Div([
-        html.Div('Tokyo', style={'backgroundColor': '#fbc02d', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
-        table2,
-        graph2,
+        html.Div('Tokyo', style={'backgroundColor': '#fbc02d', 'color': 'black'}),
+        html.Div(table2, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        html.Div(graph2, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
     ]),
     html.Div([
-        html.Div('Wellington', style={'backgroundColor': '#ffcc80', 'color': 'black', 'width': '900px', 'marginLeft': 'auto', 'marginRight': 'auto'}),
-        table3,
-        graph3,
+        html.Div('Wellington', style={'backgroundColor': '#ffcc80', 'color': 'black'}),
+        html.Div(table3, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        html.Div(graph3, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
     ]),
-    graph4,
-    graph5,
-    graph6,
-    graph7,
-    graph8
+    html.Div([
+        html.Div('All Locations', style={'backgroundColor': '#ffad8e', 'color': 'black'}),
+        html.Div(graph4, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        html.Div(graph5, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        html.Div(graph6, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        html.Div(graph7, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        html.Div(graph8, style={'width': table_graph_width, 'marginLeft': 'center', 'marginRight': 'center'}),
+        ]),
 ])
 
 if __name__ == '__main__':
